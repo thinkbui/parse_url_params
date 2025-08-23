ab76e6f3980238cad18b7270ed45a24c529d2c47
Ian Bui <thinkbui@gmail.com>
2025-08-22 23:04:50 -0500

    Updated JS to build table rows with DOM elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,28 @@ const table_body_output_id = "tbody_output";
 const table_body_output_elem = document.getElementById(table_body_output_id);
 const input_box_id = "input_box";
 const input_box_elem = document.getElementById(input_box_id);
+
+const cell_key_class = "cell_key";
+const cell_copy_class = "cell_copy";
+const cell_value_class = "cell_value";
+
+const cell_copy_btn_text = "Copy";
+
 const default_url = "https://example.com/page.php?url=http://www.example.test";
 
 let submit_button = function() {
   let raw_input = input_box_elem.value;
 
   if(raw_input) {
+    clearTableOutput();
     let url = new URL(raw_input);
     let urlParams = url.searchParams;
 
-    let tbody_output_html = buildRow("",url.origin+url.pathname);
+    table_body_output_elem.append(buildRow("", url.origin + url.pathname));
 
     for (let key of urlParams.keys()) {
-      tbody_output_html += buildRow(key,urlParams.get(key));
+      table_body_output_elem.append(buildRow(key, urlParams.get(key)));
     }
-    table_body_output_elem.innerHTML = tbody_output_html;
   } else {
     console.log("No URL to parse");
   }
@@ -24,8 +31,12 @@ let submit_button = function() {
 
 let reset_button = function() {
   input_box_elem.value = "";
-  table_body_output_elem.innerHTML = "";
+  clearTableOutput();
   input_box_elem.focus();
+}
+
+let clearTableOutput = function() {
+  table_body_output_elem.innerHTML = "";
 }
 
 let copyClick = function(elem) {
@@ -45,7 +56,45 @@ let copyClick = function(elem) {
 }
 
 let buildRow = function(k,v) {
-  return `<tr><td class='cell_key'>${k}</td><td><button onclick='copyClick(this)'>Copy</button></td><td><textarea>${v}</textarea></td></tr>`;
+  let tr_elem = document.createElement("tr");
+  tr_elem.append(buildCellKeyTd(k));
+  tr_elem.append(buildCellCopyTd());
+  tr_elem.append(buildCellValueTd(v));
+  return tr_elem;
+}
+
+let buildCellKeyTd = function(k) {
+  let td_elem = document.createElement("td");
+  td_elem.classList.add(cell_key_class);
+  td_elem.innerHTML = k;
+  return td_elem;
+}
+
+let buildCellCopyTd = function() {
+  let td_elem = document.createElement("td");
+  td_elem.classList.add(cell_copy_class);
+  td_elem.append(buildCellCopyBtn());
+  return td_elem;
+}
+
+let buildCellCopyBtn = function() {
+  let btn_elem = document.createElement("button");
+  btn_elem.innerHTML = cell_copy_btn_text;
+  btn_elem.onclick = "copyClick(this)";
+  return btn_elem;
+}
+
+let buildCellValueTd = function(v) {
+  let td_elem = document.createElement("td");
+  td_elem.classList.add(cell_value_class);
+  td_elem.append(buildCellValueTextarea(v));
+  return td_elem;
+}
+
+let buildCellValueTextarea = function(v) {
+  let textarea_elem = document.createElement("textarea");
+  textarea_elem.value = v;
+  return textarea_elem;
 }
 
 input_box_elem.value = default_url;
