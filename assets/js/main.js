244245956a5be4207ce189ab1b9b39e0382b7277
Ian Bui <thinkbui@gmail.com>
2025-08-22 21:53:50 -0500

    Updated JS to use DOM element constants

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,31 @@
-let default_url = "https://example.com/page.php?url=http://www.example.test";
+const table_body_output_id = "tbody_output";
+const table_body_output_elem = document.getElementById(table_body_output_id);
+const input_box_id = "input_box";
+const input_box_elem = document.getElementById(input_box_id);
+const default_url = "https://example.com/page.php?url=http://www.example.test";
 
 let submit_button = function() {
-  let raw_input = document.getElementById("input_box").value;
+  let raw_input = input_box_elem.value;
 
   if(raw_input) {
     let url = new URL(raw_input);
     let urlParams = url.searchParams;
+
     let tbody_output_html = buildRow("",url.origin+url.pathname);
+
     for (let key of urlParams.keys()) {
       tbody_output_html += buildRow(key,urlParams.get(key));
     }
-    document.getElementById("tbody_output").innerHTML = tbody_output_html;
+    table_body_output_elem.innerHTML = tbody_output_html;
   } else {
     console.log("No URL to parse");
   }
 }
 
 let reset_button = function() {
-  let input_box = document.getElementById("input_box");
-  input_box.value = "";
-  document.getElementById("tbody_output").innerHTML = "";
-  input_box.focus();
+  input_box_elem.value = "";
+  table_body_output_elem.innerHTML = "";
+  input_box_elem.focus();
 }
 
 let copyClick = function(elem) {
@@ -43,5 +48,5 @@ let buildRow = function(k,v) {
   return `<tr><td class='cell_key'>${k}</td><td><button onclick='copyClick(this)'>Copy</button></td><td><textarea>${v}</textarea></td></tr>`;
 }
 
-document.getElementById("input_box").value = default_url;
+input_box_elem.value = default_url;
 submit_button();
